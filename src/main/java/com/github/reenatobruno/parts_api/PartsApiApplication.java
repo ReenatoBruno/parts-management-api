19b3f28ac05ff1211e24ef1d786698f95c55726f
ReenatoBruno <renatobrunobezerra@gmail.com>
2026-05-18 23:08:01 -0300

    feat: initial project structure from Spring Initializr

---
+++ src/main/java/com/github/reenatobruno/parts_api/PartsApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.reenatobruno.parts_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/reenatobruno/parts_api/PartsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.reenatobruno.parts_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
