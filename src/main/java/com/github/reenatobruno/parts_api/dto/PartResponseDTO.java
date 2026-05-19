0a6b5dc472ee6f43e1f4421fe3a440b0a146a800
ReenatoBruno <renatobrunobezerra@gmail.com>
2026-05-19 20:18:54 -0300

    feat: Add getters to PartResponseDTO

--- src/main/java/com/github/reenatobruno/parts_api/dto/PartResponseDTO.java
+++ src/main/java/com/github/reenatobruno/parts_api/dto/PartResponseDTO.java
@@ -14,4 +14,40 @@ public class PartResponseDTO {
     private String description;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    public String getPartNumber() {
+        return partNumber;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public String getSupplier() {
+        return supplier;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
 }
