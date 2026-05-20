87e8fc24132751cfb7b35fbda36c880f9091e8c2
ReenatoBruno <renatobrunobezerra@gmail.com>
2026-05-20 16:21:58 -0300

    feat: Implement custom exception

--- src/main/java/com/github/reenatobruno/parts_api/infra/ResourceNotFoundException.java
+++ src/main/java/com/github/reenatobruno/parts_api/infra/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package com.github.reenatobruno.parts_api.infra;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
 }
