1616f4cd86c5b12a90cc22cabe61ec50b4793280
ReenatoBruno <renatobrunobezerra@gmail.com>
2026-05-20 10:59:01 -0300

    docs: add Swagger documentation into ResponseDTO

--- src/main/java/com/github/reenatobruno/parts_api/dto/PartResponseDTO.java
+++ src/main/java/com/github/reenatobruno/parts_api/dto/PartResponseDTO.java
@@ -1,53 +1,41 @@
 package com.github.reenatobruno.parts_api.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+import lombok.Getter;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Getter
+@Builder
 public class PartResponseDTO {
 
+    @Schema(description = "Part ID", example = "1")
     private Long id;
+
+    @Schema(description = "Part Number", example = "ABC-123")
     private String partNumber;
+
+    @Schema(description = "Part Description", example = "Steel bolt")
     private String name;
+
+    @Schema(description = "Part Price", example = "9.99" )
     private BigDecimal price;
+
+    @Schema(description = "Part Quantity", example = "10")
     private Integer quantity;
+
+    @Schema(description = "Part Supplier", example = "Steel Parts Inc")
     private String supplier;
+
+    @Schema(description = "Part Description", example = "Stainless steel bolt 1/4 inch")
     private String description;
+
+    @Schema(description = "Date when part was created", example = "2024-01-15T10:30:00")
     private LocalDateTime createdAt;
+
+    @Schema(description = "Date when part was last updated")
     private LocalDateTime updatedAt;
 
-    public String getPartNumber() {
-        return partNumber;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public String getSupplier() {
-        return supplier;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
 }
