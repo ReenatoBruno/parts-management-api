f99767cb01bdfee6d60b70bbd2f3293b6d3e3eaa
ReenatoBruno <renatobrunobezerra@gmail.com>
2026-05-20 16:24:02 -0300

    feat: implement Service and  PartServiceImpl with domain business logic

--- src/main/java/com/github/reenatobruno/parts_api/service/PartServiceImpl.java
+++ src/main/java/com/github/reenatobruno/parts_api/service/PartServiceImpl.java
@@ -3,6 +3,7 @@ package com.github.reenatobruno.parts_api.service;
 import com.github.reenatobruno.parts_api.dto.PartRequestDTO;
 import com.github.reenatobruno.parts_api.dto.PartResponseDTO;
 import com.github.reenatobruno.parts_api.entity.Part;
+import com.github.reenatobruno.parts_api.infra.ResourceNotFoundException;
 import com.github.reenatobruno.parts_api.mapper.PartMapper;
 import com.github.reenatobruno.parts_api.repository.PartRepository;
 import org.springframework.data.domain.Page;
@@ -11,12 +12,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class PartServiceImp implements PartService{
+public class PartServiceImpl implements PartService{
 
     private final PartRepository repository;
     private final PartMapper mapper;
 
-    public PartServiceImp(PartRepository repository, PartMapper mapper) {
+    public PartServiceImpl(PartRepository repository, PartMapper mapper) {
 
         this.repository = repository;
         this.mapper = mapper;
@@ -25,11 +26,8 @@ public class PartServiceImp implements PartService{
     @Override
     @Transactional
     public PartResponseDTO create(PartRequestDTO request) {
-
         Part part = mapper.toEntity(request);
-
         Part saved = repository.save(part);
-
         return mapper.toResponseDTO(saved);
     }
 
@@ -45,6 +43,35 @@ public class PartServiceImp implements PartService{
     public PartResponseDTO getById(Long id) {
         return repository.findById(id)
                 .map(mapper::toResponseDTO)
-                .orElseThrow(() -> new ResourceNotFoundException("Part not found with ID: " + id));
+                .orElseThrow(() -> new ResourceNotFoundException(STR."Part not found with ID: \{id}"));
+    }
+
+    @Override
+    @Transactional
+    public PartResponseDTO update(Long id, PartRequestDTO request) {
+        Part existingPart = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(STR."Cannot update. Part not found with ID: \{id}"));
+
+        existingPart.updateFields(
+                request.getPartNumber(),
+                request.getName(),
+                request.getPrice(),
+                request.getQuantity(),
+                request.getSupplier(),
+                request.getSupplier()
+        );
+
+        Part updated = repository.save(existingPart);
+
+        return mapper.toResponseDTO(updated);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException(STR."Cannot delete. Part not found with ID: \{id}");
+        }
+        repository.deleteById(id);
     }
 }
