4f2356dad393ac4c79d25d96b280aee7b5fdadc8
ReenatoBruno <renatobrunobezerra@gmail.com>
2026-06-04 02:23:48 -0300

    refactor: remove unused Sort import in PartController

--- src/main/java/com/github/reenatobruno/parts_api/controller/PartController.java
+++ src/main/java/com/github/reenatobruno/parts_api/controller/PartController.java
@@ -7,7 +7,6 @@ import com.github.reenatobruno.parts_api.service.PartService;
 import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
