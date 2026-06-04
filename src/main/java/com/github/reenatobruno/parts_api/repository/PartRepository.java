92e8697aae5eeca0d90b9e1bc6a9e1cb11534530
ReenatoBruno <renatobrunobezerra@gmail.com>
2026-06-04 01:13:44 -0300

    feat: add pagination and case-insensitive name filtering to PartRepository

--- src/main/java/com/github/reenatobruno/parts_api/repository/PartRepository.java
+++ src/main/java/com/github/reenatobruno/parts_api/repository/PartRepository.java
@@ -1,8 +1,13 @@
 package com.github.reenatobruno.parts_api.repository;
 
 import com.github.reenatobruno.parts_api.entity.Part;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PartRepository extends JpaRepository<Part, Long> {
+
     boolean existsByPartNumber(String partNumber);
+
+    Page<Part> findAllByNameContainingIgnoreCase(String partName, Pageable pageable);
 }
