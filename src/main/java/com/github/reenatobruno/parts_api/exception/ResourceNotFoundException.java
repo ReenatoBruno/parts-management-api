a9aa15b12b937d5c46f78d9e9df83406ddff0749
ReenatoBruno <renatobrunobezerra@gmail.com>
2026-06-08 17:32:49 -0300

    refactor: remove unused ResourceNotFoundException class

--- src/main/java/com/github/reenatobruno/parts_api/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.github.reenatobruno.parts_api.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
