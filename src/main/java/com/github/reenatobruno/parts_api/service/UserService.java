938f4274c6166abc8ba8de67869825a46aa2ccf9
ReenatoBruno <renatobrunobezerra@gmail.com>
2026-06-26 19:22:04 -0300

    feat: expand UserService interface with methods for user retrieval by ID and paginated listing

--- src/main/java/com/github/reenatobruno/parts_api/service/UserService.java
+++ src/main/java/com/github/reenatobruno/parts_api/service/UserService.java
@@ -2,8 +2,16 @@ package com.github.reenatobruno.parts_api.service;
 
 import com.github.reenatobruno.parts_api.dto.UserRequestDTO;
 import com.github.reenatobruno.parts_api.dto.UserResponseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
 
 public interface UserService {
 
     UserResponseDTO create(UserRequestDTO requestDTO);
+
+    UserResponseDTO getById(UUID userID);
+
+    Page<UserResponseDTO> getAll(String userName, Pageable pageable);
 }
