6f4a41f07c7f7c6ee2b328497f6436a5b81412de
ReenatoBruno <renatobrunobezerra@gmail.com>
2026-06-26 19:21:59 -0300

    feat: add paginated search by user name to UserRepository

--- src/main/java/com/github/reenatobruno/parts_api/repository/UserRepository.java
+++ src/main/java/com/github/reenatobruno/parts_api/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.github.reenatobruno.parts_api.repository;
 
 import com.github.reenatobruno.parts_api.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.nio.channels.FileChannel;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
@@ -10,4 +13,6 @@ public interface UserRepository extends JpaRepository<UserEntity, UUID> {
     boolean existsByUserCpf(String userCpf);
 
     boolean existsByUserEmail(String userEmail);
+
+    Page<UserEntity> findAllByUserNameContainingIgnoreCase(String userName, Pageable pageable);
 }
