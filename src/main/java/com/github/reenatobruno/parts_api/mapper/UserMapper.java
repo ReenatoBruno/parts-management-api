86fd0e45ce8724a556ddcd4fd919b289cb0c04fa
ReenatoBruno <renatobrunobezerra@gmail.com>
2026-06-26 01:31:49 -0300

    feat: add UserMapper to handle UserEntity and DTO transformations

---
+++ src/main/java/com/github/reenatobruno/parts_api/mapper/UserMapper.java
@@ -0,0 +1,36 @@
+package com.github.reenatobruno.parts_api.mapper;
+
+import com.github.reenatobruno.parts_api.dto.UserRequestDTO;
+import com.github.reenatobruno.parts_api.dto.UserResponseDTO;
+import com.github.reenatobruno.parts_api.dto.UserUpdateDTO;
+import com.github.reenatobruno.parts_api.entity.UserEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+    public UserEntity toEntity(UserRequestDTO requestDTO) {
+        return new UserEntity(
+                requestDTO.userName(),
+                requestDTO.userCpf(),
+                requestDTO.userEmail(),
+                requestDTO.userPassword()
+        );
+    }
+
+    public UserResponseDTO toResponse(UserEntity user) {
+        return UserResponseDTO.builder()
+                .id(user.getId())
+                .userName(user.getUserName())
+                .userEmail(user.getUserEmail())
+                .createdAt(user.getCreatedAt())
+                .updatedAt(user.getUpdatedAt())
+                .build();
+    }
+
+    public void updateEntity (UserEntity user, UserUpdateDTO updateDTO) {
+        user.updateFields(
+                updateDTO.userName(),
+                updateDTO.userEmail()
+        );
+    }
+}
