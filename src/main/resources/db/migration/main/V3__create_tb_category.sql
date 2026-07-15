7b582d99ff1c3e9565ac9fd3de0a14624a33ab08
ReenatoBruno <renatobrunobezerra@gmail.com>
2026-07-15 00:04:32 -0300

    refactor: adjust column size for created_by and updated_by in tb_category table

--- src/main/resources/db/migration/main/V3__create_tb_category.sql
+++ src/main/resources/db/migration/main/V3__create_tb_category.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS tb_categoty(
     category_active = BOOLEAN NOT NULL,
     category_created_at TIMESTAMP WITH TIME ZONE NOT NULL,
     category_updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    category_created_by VARCHAR(255) NOt NULL,
-    category_updated_by VARCHAR(255) NOT NULL,
+    category_created_by VARCHAR(50) NOt NULL,
+    category_updated_by VARCHAR(50) NOT NULL,
 
     CONSTRAINT pk_category PRIMARY KEY (category_id),
     CONSTRAINT uq_category_name UNIQUE (category_name)
