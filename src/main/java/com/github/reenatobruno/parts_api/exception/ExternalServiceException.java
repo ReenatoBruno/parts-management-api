46d0d5ad001acf24a4fd364e4e2393d710373131
ReenatoBruno <renatobrunobezerra@gmail.com>
2026-07-15 02:05:32 -0300

    feat: add ExternalServiceException for handling external service errors

---
+++ src/main/java/com/github/reenatobruno/parts_api/exception/ExternalServiceException.java
@@ -0,0 +1,7 @@
+package com.github.reenatobruno.parts_api.exception;
+
+public class ExternalServiceException extends RuntimeException {
+    public ExternalServiceException(String message) {
+        super(message);
+    }
+}
