997b1f0ebba54ebca6c5fc8bf1e35916d35f3d6b
ReenatoBruno <renatobrunobezerra@gmail.com>
2026-07-15 01:18:24 -0300

    feat: add methods to SupplierRepository for CNPJ existence check and paginated search by company name

--- src/main/java/com/github/reenatobruno/parts_api/repository/SupplierRepository.java
+++ src/main/java/com/github/reenatobruno/parts_api/repository/SupplierRepository.java
@@ -1,9 +1,15 @@
 package com.github.reenatobruno.parts_api.repository;
 
 import com.github.reenatobruno.parts_api.entity.SupplierEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.nio.channels.FileChannel;
 import java.util.UUID;
 
 public interface SupplierRepository extends JpaRepository<SupplierEntity, UUID> {
+    boolean existsByCnpj(String cnpj);
+
+    Page<SupplierEntity> findAllByCompanyNameContainingIgnoreCase(String companyName, Pageable pageable);
 }
