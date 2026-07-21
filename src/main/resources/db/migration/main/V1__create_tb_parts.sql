14956a156b23bc719e29ae293834f888eb7ce0c8
ReenatoBruno <renatobrunobezerra@gmail.com>
2026-07-21 00:24:28 -0300

    feat: add foreign key constraint for supplier_id in tb_parts table

--- src/main/resources/db/migration/main/V1__create_tb_parts.sql
+++ src/main/resources/db/migration/main/V1__create_tb_parts.sql
@@ -17,7 +17,8 @@ CREATE TABLE IF NOT EXISTS tb_parts(
     CONSTRAINT uq_part_number UNIQUE (part_number),
     CONSTRAINT chk_part_number_upper CHECK (part_number = UPPER(part_number)),
     CONSTRAINT chk_price_positive CHECK (price > 0),
-    CONSTRAINT chk_quantity_positive CHECK (quantity > 0)
+    CONSTRAINT chk_quantity_positive CHECK (quantity > 0),
+    CONSTRAINT fk_parts_supplier FOREIGN KEY (supplier_id) REFERENCES tb_supplier (supplier_id)
 );
 
 CREATE INDEX idx_part_number on tb_parts (part_number);
