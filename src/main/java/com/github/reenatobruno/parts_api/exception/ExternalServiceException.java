0186e496860f12d3f1679704e56010b114ab97aa
ReenatoBruno <renatobrunobezerra@gmail.com>
2026-07-22 21:08:20 -0300

    refactor: rename ExternalServiceException to ViaCepExternalServiceException for better specificity

--- src/main/java/com/github/reenatobruno/parts_api/exception/ExternalServiceException.java
+++
@@ -1,7 +0,0 @@
-package com.github.reenatobruno.parts_api.exception;
-
-public class ExternalServiceException extends RuntimeException {
-    public ExternalServiceException(String message) {
-        super(message);
-    }
-}
