aa61e03bfa03d87eb651b1f21e97837f01202a5d
ReenatoBruno <renatobrunobezerra@gmail.com>
2026-07-28 21:56:51 -0300

    feat: embed Address into DealershipMapper to streamline address handling and updates

--- src/main/java/com/github/reenatobruno/parts_api/mapper/DealershipMapper.java
+++ src/main/java/com/github/reenatobruno/parts_api/mapper/DealershipMapper.java
@@ -1,42 +1,32 @@
 package com.github.reenatobruno.parts_api.mapper;
 
 import com.github.reenatobruno.parts_api.dto.*;
+import com.github.reenatobruno.parts_api.entity.Address;
 import com.github.reenatobruno.parts_api.entity.DealershipEntity;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DealershipMapper {
 
-    public DealershipEntity toEntity(DealershipRequestDTO dto, ViaCepResponseDTO address) {
+    public DealershipEntity toEntity(DealershipRequestDTO dto, ViaCepResponseDTO viaCep) {
+        Address address = new Address(
+                dto.zipCode(),
+                viaCep.street(),
+                dto.number(),
+                dto.complement(),
+                viaCep.district(),
+                viaCep.city(),
+                viaCep.state()
+        );
+
         return new DealershipEntity(
                 dto.cnpj(),
                 dto.dealerName(),
                 dto.tradeName(),
                 dto.email(),
                 dto.phone(),
-                dto.zip(),
-                address.street(),
-                dto.number(),
-                dto.complement(),
-                address.district(),
-                address.city(),
-                address.state()
-        );
-    }
+                address
 
-    public DealershipAdminResponseDTO toAdminResponse(DealershipEntity entity) {
-        return new DealershipAdminResponseDTO(
-                toResponse(entity),
-                entity.getCnpj(),
-                entity.getZipCode(),
-                entity.getStreet(),
-                entity.getNumber(),
-                entity.getComplement(),
-                entity.getDistrict(),
-                entity.getCreatedAt(),
-                entity.getUpdatedAt(),
-                entity.getCreatedBy(),
-                entity.getUpdatedBy()
         );
     }
 
@@ -47,24 +37,49 @@ public class DealershipMapper {
                 .tradeName(entity.getTradeName())
                 .email(entity.getEmail())
                 .phone(entity.getPhone())
-                .city(entity.getCity())
-                .state(entity.getState())
+                .city(entity.getAddress().getCity())
+                .state(entity.getAddress().getState())
                 .build();
     }
 
-    public void updateEntity(DealershipEntity entity, DealershipUpdateDTO updateDTO, ViaCepResponseDTO address) {
+    public DealershipAdminResponseDTO toAdminResponse(DealershipEntity entity) {
+        return new DealershipAdminResponseDTO(
+                toResponse(entity),
+                entity.getCnpj(),
+                entity.getAddress().getZipCode(),
+                entity.getAddress().getStreet(),
+                entity.getAddress().getNumber(),
+                entity.getAddress().getComplement(),
+                entity.getAddress().getDistrict(),
+                entity.getCreatedAt(),
+                entity.getUpdatedAt(),
+                entity.getCreatedBy(),
+                entity.getUpdatedBy()
+        );
+    }
+
+    public void updateEntity(DealershipEntity entity, DealershipUpdateDTO updateDTO, ViaCepResponseDTO viaCep) {
+        Address address = null;
+
+        if (viaCep != null) {
+            address = new Address(
+                    updateDTO.zipCode(),
+                    viaCep.street(),
+                    updateDTO.number(),
+                    updateDTO.complement(),
+                    viaCep.district(),
+                    viaCep.city(),
+                    viaCep.state()
+            );
+        }
+
         entity.updateFields(
                 updateDTO.dealerName(),
                 updateDTO.tradeName(),
                 updateDTO.email(),
                 updateDTO.phone(),
-                updateDTO.zip(),
-                address != null ? address.street() : null,
-                updateDTO.number(),
-                updateDTO.complement(),
-                address != null ? address.district() : null,
-                address != null ? address.city() : null,
-                address != null ? address.state() : null
+                address
+
         );
     }
 }
