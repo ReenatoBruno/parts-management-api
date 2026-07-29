1883a7b4129ee364d588a3e098ac08801d9a56e5
ReenatoBruno <renatobrunobezerra@gmail.com>
2026-07-28 23:24:16 -0300

    chore: fix column types and adjust lengths in category table creation script

--- src/main/resources/db/migration/main/V2__create_tb_category.sql
+++ src/main/resources/db/migration/main/V2__create_tb_category.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS tb_categoty(
 
     category_id UUID NOT NULL,
-    category_name VARCHAR(60) NOT NULL,
+    category_name VARCHAR(50) NOT NULL,
     category_description VARCHAR(255),
     category_active = BOOLEAN NOT NULL,
     category_created_at TIMESTAMP WITH TIME ZONE NOT NULL,
     category_updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
-    category_created_by VARCHAR(50) NOt NULL,
-    category_updated_by VARCHAR(50) NOT NULL,
+    category_created_by VARCHAR(255) NOt NULL,
+    category_updated_by VARCHAR(255) NOT NULL,
 
     CONSTRAINT pk_category PRIMARY KEY (category_id),
     CONSTRAINT uq_category_name UNIQUE (category_name)
